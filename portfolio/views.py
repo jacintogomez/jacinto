938e7ec90a9624f07e66a5a75c8c7196e8d9f8c5
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2024-02-13 13:33:00 -0500

    AWS changes

--- portfolio/views.py
+++ portfolio/views.py
@@ -69,21 +69,22 @@ def work_experience_page(request):
     return render(request, "workexperience.html", {})
 
 def contact_form(request):
-    if request.method == 'POST':
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            form.save()
-            subject = 'New feedback from Personal Website'
-            message = f'Name: {form.cleaned_data["name"]}\nEmail: {form.cleaned_data["email"]}\n\nNote:\n{form.cleaned_data["note"]}'
-            from_email = settings.EMAIL_HOST_USER
-            recipient_list = [settings.CONTACT_EMAIL]
-            print('contacted user is this -------->', settings.CONTACT_EMAIL)
-            # sending
-            send_mail(subject, message, from_email, recipient_list)
-            return redirect('thank_you')
-    else:
-        form = ContactForm()
-    return render(request, 'contact_form.html', {'form': form})
+    # if request.method == 'POST':
+    #     form = ContactForm(request.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #         subject = 'New feedback from Personal Website'
+    #         message = f'Name: {form.cleaned_data["name"]}\nEmail: {form.cleaned_data["email"]}\n\nNote:\n{form.cleaned_data["note"]}'
+    #         from_email = settings.EMAIL_HOST_USER
+    #         recipient_list = [settings.CONTACT_EMAIL]
+    #         print('contacted user is this -------->', settings.CONTACT_EMAIL)
+    #         # sending
+    #         send_mail(subject, message, from_email, recipient_list)
+    #         return redirect('thank_you')
+    # else:
+    #     form = ContactForm()
+    # return render(request, 'contact_form.html', {'form': form})
+    pass
 
 
 def thank_you_page(request):
