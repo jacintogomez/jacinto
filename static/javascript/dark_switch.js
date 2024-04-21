14f5647eb3052c31f06a83ed5a441b425cba3d25
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2024-04-20 21:31:42 -0400

    dark mode extended to other pages via browser state

--- static/javascript/dark_switch.js
+++ static/javascript/dark_switch.js
@@ -1,9 +1,27 @@
 let sheet=document.getElementById('styles');
+let lightlink="../static/portfolio/styles.css";
+let darklink="../static/portfolio/dark.css";
+let isdark=(localStorage.getItem('darkmode')===true);
 
 function switchmode(){
     if(sheet.href.includes('styles.css')){
-        sheet.href="static/portfolio/dark.css";
+        sheet.href=darklink;
+        localStorage.setItem('darkmode',true);
+        isdark=true;
     }else{
-        sheet.href="static/portfolio/styles.css";
+        sheet.href=lightlink;
+        localStorage.setItem('darkmode',false);
+        isdark=false;
     }
 }
+
+function currentsheet(){
+    console.log(document.title+' is now '+isdark);
+    if(isdark||document.title==='About'){
+        sheet.href=darklink;
+    }else{
+        sheet.href=lightlink;
+    }
+}
+
+currentsheet();
