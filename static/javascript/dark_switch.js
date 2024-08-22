dd02b3a2b9b3046437b5f959c97a0e2b4b538348
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2024-08-21 23:22:37 -0400

    dark mode moon emoji, and light mode sun emoji

--- static/javascript/dark_switch.js
+++ static/javascript/dark_switch.js
@@ -2,15 +2,16 @@ let sheet=document.getElementById('mode');
 let lightlink="../static/portfolio/light.css";
 let darklink="../static/portfolio/dark.css";
 let isdark=(localStorage.getItem('darkmode')==="true");
+let emoji=document.getElementById('drk');
 
 function switchmode(){
     if(sheet.href.includes('light.css')){
-        setTimeout(function(){
-            sheet.href=darklink;
-            localStorage.setItem('darkmode',true);
-            isdark=true;
-        },2500);
+        emoji.innerHTML='𖤓';
+        sheet.href=darklink;
+        localStorage.setItem('darkmode',true);
+        isdark=true;
     }else{
+        emoji.innerHTML='☾';
         sheet.href=lightlink;
         localStorage.setItem('darkmode',false);
         isdark=false;
